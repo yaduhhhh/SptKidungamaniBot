08aaa7a4992427c4f8ccc516c212d53534490ada
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-18 16:12:34 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -5,8 +5,7 @@ GROUPS = [
         {
             'id': -1001935409928,
             'name': '🔞𝐊𝐃𝐌 𝐔𝐋𝐓𝐑𝐀🌼 1.MONTH',
-            'price': ’350’,
-            'price': ’650’,
+            'price': 350,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                             
             
