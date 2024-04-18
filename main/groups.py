2981052db7680bf5468afdbd9b7c20900cd35a0d
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-19 03:40:48 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -7,8 +7,7 @@ GROUPS = [
             'name': '🔞𝐊𝐃𝐌 𝐔𝐋𝐓𝐑𝐀🌼 1.MONTH',
             'price': 350,
             'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                             
-            
+            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                              
         },
         {
             'id': -1001935409928,
@@ -61,5 +60,14 @@ GROUPS = [
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/973ea89b4880a082c0a5c.jpg']                                                             
             
+        }
+    ]
+GROUP_X = [
+        {
+            'id': -1001935409928,
+            'name': '🔞𝐊𝐃𝐌 𝐔𝐋𝐓𝐑𝐀🌼 1.MONTH',
+            'price': 350,
+            'paynote': 'Ultra',
+            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']
         }
     ]
