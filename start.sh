3bb8f099978937fcad1304e2ad630eac68cb7cf5
forwid6@gmail.com <forwid6@gmail.com>
2024-04-18 09:31:35 +0530

    new token

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ apt update && apt upgrade -y
 apt install git -y           
 pip install -U pip
 
-git clone https://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@github.com/yaduhhhh/SptKidungamaniBot kidu                     
+git clone https://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@github.com/yaduhhhh/SptKidungamaniBot kidu                     
 cd kidu
 pip install -U -r requirements.txt
 python3 bot.py
