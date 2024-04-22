a9c60b248c93f27b0a2f12f1e80cd5c9537925c0
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-23 01:13:56 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -4,28 +4,28 @@
 GROUPS = [
         {
             'id': -1001935409928,
-            'name': '𝟏.𝗨𝗟𝗧𝗥𝗔 Kidungamani',
+            'name': '𝟏. 𝗨𝗟𝗧𝗥𝗔 Kidungamani',
             'price': 650,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                              
         },
         {
             'id': -1002061016678,
-            'name': '𝟐.𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
+            'name': '𝟐. 𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
             'price': 500,
             'paynote': 'Ultra',
             'pics': ['https://telegra.ph/file/973ea89b4880a082c0a5c.jpg', 'https://telegra.ph/file/ded94bafb8644a7c83974.jpg']
         },
         {
             'id': -1001945623510,
-            'name': '𝟑.𝗡𝗶𝗹𝗮 𝗡𝗮𝗺𝗯𝗶𝗮𝗿',
+            'name': '𝟑. 𝗡𝗶𝗹𝗮 𝗡𝗮𝗺𝗯𝗶𝗮𝗿',
             'price': 400,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/8aae552e09d3cac73d6f3.jpg']
         },
         {
             'id': -1002146607794,
-            'name': '𝟒.𝗧𝗮𝗻𝗴𝗼 𝗩𝗶𝗱𝗲𝗼𝘀',
+            'name': '𝟒. 𝗧𝗮𝗻𝗴𝗼 𝗩𝗶𝗱𝗲𝗼𝘀',
             'price': 300,
                 'price': 300,
             'paynote': 'tango',
@@ -33,14 +33,14 @@ GROUPS = [
         },
         {
             'id': -1002091217696,
-            'name': '𝟓.𝗪𝗲𝗯𝘀𝗲𝗿𝗶𝗲𝘀',
+            'name': '𝟓. 𝗪𝗲𝗯𝘀𝗲𝗿𝗶𝗲𝘀',
             'price': 300,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/2986260c5201fcc3d7b06.jpg']
         },
         {
             'id': -1002115434066,
-            'name': '𝟔.𝗙𝗼𝗿𝗲𝗶𝗴𝗻',
+            'name': '𝟔. 𝗙𝗼𝗿𝗲𝗶𝗴𝗻',
             'price': 400,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/d388bbe7d05d02b611e68.jpg']
