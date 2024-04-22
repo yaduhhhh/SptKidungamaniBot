f2296d6ec32e125f34add830b71d75e305cd0972
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-22 18:26:41 +0530

    Update start.py

--- main/start.py
+++ main/start.py
@@ -67,7 +67,7 @@ async def cb_func(client, query):
         group_id = int(data.split('+', 1)[1])
         grp_data = GROUPS[group_id]
         btn = [[
-                InlineKeyboardButton(('+', 1), "🔞BUY GROUP🔞", url='https://kidungamani.com/ultra'),
+                InlineKeyboardButton("🔞BUY GROUP🔞", url='https://kidungamani.com/ultra'),
                 ],[
                 InlineKeyboardButton("DEMO ᴩɪᴄꜱ 🫦", f'pics+{group_id}')
                 ],[
