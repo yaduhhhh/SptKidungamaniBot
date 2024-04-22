6069cc87278331dee85bfa54234198cc36ffcc49
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-23 01:11:12 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -4,7 +4,7 @@
 GROUPS = [
         {
             'id': -1001935409928,
-            'name': '🌺𝐂𝐎𝐌𝐁𝐎 𝟏 𝐘𝐄𝐀𝐑',
+            'name': '🌺𝐂𝐎𝐌𝐁𝐎',
             'price': 1999,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/973ea89b4880a082c0a5c.jpg']                                                             
@@ -12,38 +12,15 @@ GROUPS = [
         },
         {
             'id': -1001935409928,
-            'name': '🌼𝐂𝐎𝐌𝐁𝐎 𝟏 𝐌𝐎𝐍𝐓𝐇',
-            'price': 999,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/973ea89b4880a082c0a5c.jpg']                                                             
-            
-        },
-        {
-            'id': -1001935409928,
-            'name': '🔴 𝟏.𝗨𝗟𝗧𝗥𝗔 Kidungamani',
+            'name': '𝟏.𝗨𝗟𝗧𝗥𝗔 Kidungamani',
             'price': 650,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                              
         },
         {
-            'id': -1001935409928,
-            'name': '🟢 𝟏.𝗨𝗟𝗧𝗥𝗔 Kidungamani',
-            'price': 350,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                             
-            
-        },
-        {
             'id': -1002061016678,
-            'name': '🔴 𝟐.𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
+            'name': '𝟐.𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
             'price': 500,
-            'paynote': 'Ultra',
-            'pics': ['https://telegra.ph/file/973ea89b4880a082c0a5c.jpg', 'https://telegra.ph/file/ded94bafb8644a7c83974.jpg']
-        },
-        {
-            'id': -1002061016678,
-            'name': '🟢 𝟐.𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
-            'price': 250,
             'paynote': 'Ultra',
             'pics': ['https://telegra.ph/file/973ea89b4880a082c0a5c.jpg', 'https://telegra.ph/file/ded94bafb8644a7c83974.jpg']
         },
@@ -51,13 +28,6 @@ GROUPS = [
             'id': -1001945623510,
             'name': '🔴 𝟑.𝗡𝗶𝗹𝗮 𝗡𝗮𝗺𝗯𝗶𝗮𝗿',
             'price': 400,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/8aae552e09d3cac73d6f3.jpg']
-        },
-        {
-            'id': -1001945623510,
-            'name': '🟢 𝟑.𝗡𝗶𝗹𝗮 𝗡𝗮𝗺𝗯𝗶𝗮𝗿',
-            'price': 200,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/8aae552e09d3cac73d6f3.jpg']
         },
@@ -70,24 +40,9 @@ GROUPS = [
             'pics': ['https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/c66654d35a8dbc4b0b22e.jpg']
         },
         {
-            'id': -1002146607794,
-            'name': '🟢 𝟒.𝗧𝗮𝗻𝗴𝗼 𝗩𝗶𝗱𝗲𝗼𝘀',
-            'price': 100,
-                'price': 100,
-            'paynote': 'tango',
-            'pics': ['https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/c66654d35a8dbc4b0b22e.jpg']
-        },
-        {
             'id': -1002091217696,
             'name': '🔴 𝟓.𝗪𝗲𝗯𝘀𝗲𝗿𝗶𝗲𝘀',
             'price': 300,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/2986260c5201fcc3d7b06.jpg']
-        },
-        {
-            'id': -1002091217696,
-            'name': '🟢 𝟓.𝗪𝗲𝗯𝘀𝗲𝗿𝗶𝗲𝘀',
-            'price': 100,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/2986260c5201fcc3d7b06.jpg']
         },
@@ -98,11 +53,4 @@ GROUPS = [
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/d388bbe7d05d02b611e68.jpg']
         },
-        {
-            'id': -1002115434066,
-            'name': '🟢 𝟔.𝗙𝗼𝗿𝗲𝗶𝗴𝗻',
-            'price': 200,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/d388bbe7d05d02b611e68.jpg']
-        }
     ]
