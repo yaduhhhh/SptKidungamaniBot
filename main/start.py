5f0d5384f242441eae57a2d639b28a48f4d767db
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-23 03:36:01 +0530

    Update start.py

--- main/start.py
+++ main/start.py
@@ -67,7 +67,7 @@ async def cb_func(client, query):
         group_id = int(data.split('+', 1)[1])
         grp_data = GROUPS[group_id]
         btn = [[
-                InlineKeyboardButton(f"ᴩᴀʏ {grp_data['price']}₹", f"{grp_data['url']}"),
+                InlineKeyboardButton(f"ᴩᴀʏ {grp_data['price']}₹", f"buy+{grp_data['url']}"),
                 ],[
                 InlineKeyboardButton("DEMO ᴩɪᴄꜱ 🫦", f'pics+{group_id}')
                 ],[
@@ -96,7 +96,7 @@ async def cb_func(client, query):
         ]])
        
         txt = Txt.PAY_TEXT.format(price=grp_data['price'], upi=Config.UPI_ID)
-        await query.edit_message_media(InputMediaPhoto(Config.QR_CODE, txt, enums.ParseMode.HTML), btn)
+        await query.edit_message_media(InputMediaPhoto(Config.QR_CODE='https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', txt, enums.ParseMode.HTML), btn)
         proof = await client.listen_message(user_id) #, filters=filters.photo)
         if proof.text:
             if proof.text == '/cancel':
