e25f5244fe712511b89f20c0f688386bb6381e92
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-23 02:29:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     QR_CODE = "https://graph.org/file/a927c5c5c24622eb95462.jpg"
     UPI_ID = "vip8kidungamani@axl"
     TUTORIAL = "BAACAgUAAxkBAAImjGX6ixng9CxtwE3hzgGJ-Mnvt5wMAAIXCwACxdjQV1FJxY3H2F1HHgQ"
-    URL_LINK = "https://kidungamani.com"
+    URL = "https://kidungamani.com"
    
 
 class Txt:
