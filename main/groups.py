7ce6f0b8b96636f97ef20a101e2dfad90daa6e30
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-06-16 03:07:16 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -11,45 +11,9 @@ GROUPS = [
         },
         {
             'id': -1002061016678,
-            'name': '𝟐. 𝗥𝗲𝘀𝗵𝗺𝗶 𝗡𝗮𝗶𝗿',
+            'name': '𝟐. 🌟𝗨𝗟𝗧𝗥𝗔 Kidungamani',
             'price': 250,
             'paynote': 'Ultra',
             'pics': ['https://telegra.ph/file/973ea89b4880a082c0a5c.jpg', 'https://telegra.ph/file/ded94bafb8644a7c83974.jpg']
-        },
-        {
-            'id': -1001945623510,
-            'name': '𝟑. 𝗡𝗶𝗹𝗮 𝗡𝗮𝗺𝗯𝗶𝗮𝗿',
-            'price': 200,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/8aae552e09d3cac73d6f3.jpg']
-        },
-        {
-            'id': -1002146607794,
-            'name': '𝟒. 𝗧𝗮𝗻𝗴𝗼 𝗩𝗶𝗱𝗲𝗼𝘀',
-            'price': 200,
-            'paynote': 'tango',
-            'pics': ['https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/c66654d35a8dbc4b0b22e.jpg']
-        },
-        {
-            'id': -1002091217696,
-            'name': '𝟓. 𝗪𝗲𝗯𝘀𝗲𝗿𝗶𝗲𝘀',
-            'price': 200,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/2986260c5201fcc3d7b06.jpg']
-        },
-        {
-            'id': -1002115434066,
-            'name': '𝟔. 𝗙𝗼𝗿𝗲𝗶𝗴𝗻',
-            'price': 200,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/d388bbe7d05d02b611e68.jpg']
-        },
-        {
-            'id': -1001935409928,
-            'name': '🌺𝐂𝐎𝐌𝐁𝐎',
-            'price': 999,
-            'paynote': 'Ultra',
-            'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/9a80686fff389d83fc37b.jpg', 'https://graph.org/file/52c458e50c0ae24236bd2.jpg', 'https://graph.org/file/4652ece5d300948417c2f.jpg', 'https://graph.org/file/5b892a958d7b5657bb48c.jpg', 'https://graph.org/file/973ea89b4880a082c0a5c.jpg']                                                             
-            
         }
     ]
