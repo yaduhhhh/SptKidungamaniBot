714772128152303e0196b33b87b44166a232cff6
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-06-27 01:25:21 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -8,12 +8,5 @@ GROUPS = [
             'price': 350,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                              
-        },
-        {
-            'id': -1002061016678,
-            'name': '𝟐. 🌟𝗨𝗟𝗧𝗥𝗔 Kidungamani',
-            'price': 250,
-            'paynote': 'Ultra',
-            'pics': ['https://telegra.ph/file/973ea89b4880a082c0a5c.jpg', 'https://telegra.ph/file/ded94bafb8644a7c83974.jpg']
         }
     ]
