970d513b23c1556297138755a7797036d34a9d7b
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-06-27 01:48:56 +0530

    Update groups.py

--- main/groups.py
+++ main/groups.py
@@ -4,7 +4,7 @@
 GROUPS = [
         {
             'id': -1002244534037,
-            'name': '𝟏. 🌟𝗨𝗟𝗧𝗥𝗔 Kidungamani',
+            'name': '🌟𝗨𝗟𝗧𝗥𝗔 Kidungamani',
             'price': 350,
             'paynote': 'Ultra',
             'pics': ['https://graph.org/file/58a143c3adc6aa70b3a1b.jpg', 'https://graph.org/file/2c423c90cf9051fd3d07f.jpg']                                                              
